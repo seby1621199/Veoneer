afb2393a24c4f9476200aab89f33529bfe7fc3c3
sebi <sebastian-ioan.paval@student.tuiasi.ro>
2023-05-08 13:36:17 +0300

    Fixing multiple presses & Move all in one window.

--- Threads & Tasks/WpfApp1/Afisare.xaml.cs
+++ Threads & Tasks/WpfApp1/Afisare.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,43 @@ namespace WpfApp1
     /// </summary>
     public partial class Afisare : Window
     {
+        private bool _isActive { get; set; } = false;
         public Afisare()
         {
             InitializeComponent();
+        }
+        private void NormalExecution()
+        {
+            textBlock.Text = String.Empty;
+
+            textBlock.Text += "Start Normal\n";
+            Thread.Sleep(5000);
+            textBlock.Text += "End Normal\n";
+        }
+
+        private async Task AsyncExecution()
+        {
+            if (!_isActive)
+            {
+                _isActive = true;
+                textBlock.Text = String.Empty;
+
+                textBlock.Text += "Start Async \n";
+                await Task.Delay(10000);
+                textBlock.Text += "End Async\n";
+                _isActive = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NormalExecution();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            await AsyncExecution();
+
         }
     }
 }
